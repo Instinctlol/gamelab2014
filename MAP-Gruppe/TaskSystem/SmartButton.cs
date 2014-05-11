5a1a2393c6c058abf497ac778a4ab0b8c5483d65
m_scho52@IVV5NET.WWU.DE <m_scho52@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-05-11 15:53:34 +0000

    TaskSystem: Grundgerüst aufgestellt. Tasks fehlen noch hört beim Button auf zurzeit. Interaktion mit Repairables fehlt noch.

---
+++ MAP-Gruppe/TaskSystem/SmartButton.cs
@@ -0,0 +1,73 @@
+﻿using Engine;
+using Engine.MathEx;
+using Engine.UISystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEntities
+{
+    public class SmartButton : Button
+    {
+
+        public delegate void PressedDelegate(SmartButton entity);
+
+        [LogicSystemBrowsable(true)]
+        public event PressedDelegate Pressed;
+
+
+
+        protected override void OnAttach()
+        {
+            base.OnAttach();
+
+
+            SoundClick = "Sounds/ButtonClick.ogg";
+            SoundMouseOver = "Sounds/ButtonMouseIntoArea.ogg";
+
+            this.Click += SmrtClick;
+        }
+
+        void SmrtClick( Button b )
+        {
+            if (Pressed != null)
+                Pressed(this);
+        }
+
+
+        public void AttachRepairable(Repairable r)
+        {
+            if (r != null)
+            {
+                r.Repair += new Repairable.RepairDelegate(OnRepair);
+                OnRepair(r);
+            }
+        }
+
+        public void DetachRepairable(Repairable r)
+        {
+            if (r != null)
+            {
+                r.Repair -= new Repairable.RepairDelegate(OnRepair);
+                OnRepair(r);
+            }
+        }
+
+
+        public void OnRepair(Repairable r)
+        {
+            if (r.Repaired)
+            {
+                Enable = true;
+                Visible = true;
+            }
+            else
+            {
+                Enable = false;
+                Visible = false;
+            }
+        }
+
+
+    }
+}
---
+++ MAP-Gruppe/TaskSystem/Terminal.cs
@@ -0,0 +1,136 @@
+﻿using Engine;
+using Engine.MapSystem;
+using Engine.UISystem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Text;
+
+namespace ProjectEntities
+{
+
+    public class TerminalType : DynamicType
+    {
+    }
+
+
+    public class Terminal : Dynamic
+    {
+
+        TerminalType _type = null; public new TerminalType Type { get { return _type; } }
+
+        [FieldSerialize]
+        private Repairable repairable = null;
+
+        [FieldSerialize]
+        private SmartButton initialButton = new SmartButton();
+
+
+
+        private MapObjectAttachedGui attachedGuiObject;
+        private In3dControlManager controlManager;
+        private Control mainControl;
+
+        public delegate void TerminalActionDelegate(Terminal entity);
+
+        [LogicSystemBrowsable(true)]
+        public event TerminalActionDelegate TerminalAction;
+
+        [Browsable(false)]
+        [LogicSystemBrowsable(true)]
+        public In3dControlManager ControlManager
+        {
+            get { return controlManager; }
+        }
+
+        [Browsable(false)]
+        [LogicSystemBrowsable(true)]
+        public Control MainControl
+        {
+            get { return mainControl; }
+            set { mainControl = value; }
+        }
+
+        public Repairable Repairable
+        {
+            get { return repairable; }
+            set { repairable = value; }
+        }
+
+        /*
+        [Editor(typeof(EditorGuiUITypeEditor), typeof(UITypeEditor))]
+        public string InitialButton
+        {
+            get { return initialButton; }
+            set
+            {
+                if (initialButton != null)
+                {
+                    initialButton.DetachRepairable(Repairable);
+                    initialButton.Pressed -= new SmartButton.PressedDelegate(OnButtonPressed);
+                }
+
+                initialButton = value;
+
+                if (initialButton != null)
+                {
+                    initialButton.AttachRepairable(Repairable);
+                    initialButton.Pressed += new SmartButton.PressedDelegate(OnButtonPressed);
+                }
+
+                CreateMainControl();
+            }
+        }*/
+
+        protected override void OnPostCreate(bool loaded)
+        {
+            base.OnPostCreate(loaded);
+
+            foreach (MapObjectAttachedObject attachedObject in AttachedObjects)
+            {
+                attachedGuiObject = attachedObject as MapObjectAttachedGui;
+                if (attachedGuiObject != null)
+                {
+                    controlManager = attachedGuiObject.ControlManager;
+                    break;
+                }
+            }
+            initialButton.Pressed += new SmartButton.PressedDelegate(OnButtonPressed);
+            initialButton.AttachRepairable(repairable);
+            CreateMainControl();
+        }
+
+        protected override void OnDestroy()
+        {
+            mainControl = null;
+            controlManager = null;
+            base.OnDestroy();
+        }
+
+        public void OnButtonPressed(SmartButton b)
+        {
+            if (TerminalAction != null)
+                TerminalAction(this);
+        }
+
+        void CreateMainControl()
+        {
+            if (mainControl != null)
+            {
+                mainControl.Parent.Controls.Remove(mainControl);
+                mainControl = null;
+            }
+
+            if (controlManager != null && initialButton != null)
+            {
+                mainControl = initialButton;
+                if (mainControl != null)
+                    controlManager.Controls.Add(mainControl);
+            }
+
+            //update MapBounds
+            SetTransform(Position, Rotation, Scale);
+        }
+    }
+}
