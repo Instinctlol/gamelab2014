3928dcc01b118e8be352d53c869bb17f7f6f03bd
n_temm01@IVV5NET.WWU.DE <n_temm01@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-05-28 13:21:26 +0000

    Grid in die Map eingefügt Computer.cs hinzugefügt

---
+++ Projekt/Src/ProjectEntities/Alien Specific/Computer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEntities
+{
+    public class ComputerType : DynamicType
+    {
+    }
+
+    /// <summary>
+    /// Main computer that can be controlled by the boss alien and
+    /// the astronauts
+    /// </summary>
+    class Computer : Dynamic
+    {
+        ComputerType _type = null; public new ComputerType Type { get { return _type; } }
+
+        /// <summary>
+        /// Show the state of the station, e.g. how many life the astronauts 
+        /// still have, how the rings are positioned...
+        /// </summary>
+        public void ShowState()
+        {
+
+        }
+
+        /// <summary>
+        /// Do one rotation of one ring to the left or to the right side.
+        /// </summary>
+        /// <param name="ringNumber"></param>
+        /// <param name="left"></param>
+        public void RotateRing(int ringNumber, bool left)
+        {
+
+        }
+
+        /// <summary>
+        /// Switches off or on the power of one sector (room)
+        /// </summary>
+        /// <param name="on"></param>
+        public void SetSectorPower(bool on)
+        {
+
+        }
+    }
+}
