37ae7e9edd233688e07d2c2b7cd26660af08d83a
j_wiel04@IVV5NET.WWU.DE <j_wiel04@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-06-02 13:55:04 +0000

    noch was vergessen

---
+++ Projekt/Src/ProjectEntities/LightActionWindow.cs
@@ -0,0 +1,35 @@
+﻿using Engine.UISystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace ProjectEntities
+{
+    public class LightActionWindow : Window
+    {
+        //Textfeld in dem ausgegeben werden soll
+        private TextBox countdownBox;
+        private Button switchButton;
+        private Sector currSector;
+
+        public LightActionWindow(Terminal terminal)
+        {
+            //GUI erzeugen
+            CurWindow = ControlDeclarationManager.Instance.CreateControl("GUI\\ActionWindows\\LightActionGUI.gui");
+
+            countdownBox = (TextBox)CurWindow.Controls["Output"];
+            switchButton = ((Button)CurWindow.Controls["SwitchButton"]);
+
+            //Methode anmelden
+            switchButton.Click += SwitchButton_click;
+
+            currSector = StationSystem.Instance.GetSector(terminal.Position);
+        }
+
+        private void SwitchButton_click(Button sender)
+        {
+            currSector.SwitchLights(!currSector.LightStatus);
+        }
+    }
+}
