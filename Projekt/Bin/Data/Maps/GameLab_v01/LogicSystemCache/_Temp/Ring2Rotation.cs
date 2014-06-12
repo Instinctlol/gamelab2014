4a5f08e412732c18268540b930c67b921aea4281
j_wiel04@IVV5NET.WWU.DE <j_wiel04@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-06-12 13:19:53 +0000

    GUI angepasst fuer Oculus und Terminal Action Type getestet mit Logic Editor -> Funktioniert alles

---
+++ Projekt/Bin/Data/Maps/GameLab_v01/LogicSystemCache/_Temp/Ring2Rotation.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine;
+using Engine.FileSystem;
+using Engine.MathEx;
+using Engine.Utils;
+using Engine.Renderer;
+using Engine.PhysicsSystem;
+using Engine.SoundSystem;
+using Engine.UISystem;
+using Engine.EntitySystem;
+using Engine.MapSystem;
+using ProjectCommon;
+using ProjectEntities;
+
+namespace Maps_GameLab_v___LogicSystem_LogicSystemScripts
+{
+	public class Ring2Rotation : Engine.EntitySystem.LogicSystem.LogicEntityObject
+	{
+		ProjectEntities.Terminal __ownerEntity;
+		
+		public Ring2Rotation( ProjectEntities.Terminal ownerEntity )
+			: base( ownerEntity )
+		{
+			this.__ownerEntity = ownerEntity;
+			ownerEntity.TerminalRotateLeftAction += delegate( ProjectEntities.Terminal __entity ) { if( Engine.EntitySystem.LogicSystemManager.Instance != null )TerminalRotateLeftAction(  ); };
+			ownerEntity.TerminalRotateRightAction += delegate( ProjectEntities.Terminal __entity ) { if( Engine.EntitySystem.LogicSystemManager.Instance != null )TerminalRotateRightAction(  ); };
+		}
+		
+		public ProjectEntities.Terminal Owner
+		{
+			get { return __ownerEntity; }
+		}
+		
+		
+		public void TerminalRotateLeftAction()
+		{
+			((Ring)Entities.Instance.GetByName("F2_Ring")).RotateLeft();
+		}
+
+		public void TerminalRotateRightAction()
+		{
+			((Ring)Entities.Instance.GetByName("F2_Ring")).RotateRight();
+		}
+
+	}
+}
