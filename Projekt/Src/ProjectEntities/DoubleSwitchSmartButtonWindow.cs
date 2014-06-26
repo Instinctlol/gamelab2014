0c5f722f80500bab963cb9ac39e9f8a800997d36
j_wiel04@IVV5NET.WWU.DE <j_wiel04@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-06-26 13:54:59 +0000

    Smartbutton zeugs

--- Projekt/Src/ProjectEntities/DoubleSwitchSmartButtonWindow.cs
+++ Projekt/Src/ProjectEntities/DoubleSwitchSmartButtonWindow.cs
@@ -1,11 +1,56 @@
-﻿using System;
+﻿using Engine.UISystem;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ProjectEntities
 {
-    class DoubleSwitchSmartButtonWindow
+    class DoubleSwitchSmartButtonWindow : SmartButtonWindow
     {
+        enum NetworkMessages
+        {
+            LightSwitchButtonClick,
+            DoorSwitchButtonClick
+        }
+
+        public DoubleSwitchSmartButtonWindow(SmartButton button)
+            : base(button)
+        {
+            CurWindow = ControlDeclarationManager.Instance.CreateControl("GUI\\ActionWindows\\DoubleSwitchActionGUI.gui");
+            ((Button)CurWindow.Controls["LightSwitchButton"]).Click += LightSwitchLeftButton_Click;
+            ((Button)CurWindow.Controls["DoorSwitchButton"]).Click += DoorSwitchButton_Click;
+            ((Button)CurWindow.Controls["LightSwitchButton"]).Click += SmartClick;
+            ((Button)CurWindow.Controls["DoorSwitchButton"]).Click += SmartClick;
+
+            button.Server_WindowDataReceived += Server_WindowDataReceived;
+        }
+
+        private void Server_WindowDataReceived(ushort message)
+        {
+            if (button.IsActive)
+            {
+                NetworkMessages msg = (NetworkMessages)message;
+                switch (msg)
+                {
+                    case NetworkMessages.LightSwitchButtonClick:
+                        button.Terminal.DoRotateLeftEvent();
+                        break;
+                    case NetworkMessages.DoorSwitchButtonClick:
+                        button.Terminal.DoRotateRightEvent();
+                        break;
+                }
+            }
+        }
+
+        private void LightSwitchLeftButton_Click(Button sender)
+        {
+            button.Client_SendWindowData((UInt16)NetworkMessages.LightSwitchButtonClick);
+        }
+
+        private void DoorSwitchButton_Click(Button sender)
+        {
+            button.Client_SendWindowData((UInt16)NetworkMessages.DoorSwitchButtonClick);
+        }
     }
 }
---
+++ Projekt/Src/ProjectEntities/RotateAndSingleSwitchSmartButtonWindow.cs
@@ -0,0 +1,70 @@
+﻿using Engine.UISystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEntities
+{
+    class RotateAndSingleSwitchSmartButtonWindow : SmartButtonWindow
+    {
+        enum NetworkMessages
+        {
+            RotateLeftButtonClick,
+            RotateRightButtonClick,
+            SwitchButtonClick
+        }
+
+        public RotateAndSingleSwitchSmartButtonWindow(SmartButton button)
+            : base(button)
+        {
+            CurWindow = ControlDeclarationManager.Instance.CreateControl("GUI\\ActionWindows\\RotateAndSingleSwitchActionGUI.gui");
+            ((Button)CurWindow.Controls["LeftButton"]).Click += RotateLeftButton_Click;
+            ((Button)CurWindow.Controls["RightButton"]).Click += RotateRightButton_Click;
+            ((Button)CurWindow.Controls["LeftButton"]).Click += SmartClick;
+            ((Button)CurWindow.Controls["RightButton"]).Click += SmartClick;
+            ((Button)CurWindow.Controls["SwitchButton"]).Click += SwitchButton_Click;
+            ((Button)CurWindow.Controls["SwitchButton"]).Click += SmartClick;
+
+            button.Server_WindowDataReceived += Server_WindowDataReceived;
+        }
+
+        private void Server_WindowDataReceived(ushort message)
+        {
+            if (button.IsActive)
+            {
+                NetworkMessages msg = (NetworkMessages)message;
+                switch (msg)
+                {
+                    case NetworkMessages.RotateLeftButtonClick:
+                        button.Terminal.DoRotateLeftEvent();
+                        break;
+                    case NetworkMessages.RotateRightButtonClick:
+                        button.Terminal.DoRotateRightEvent();
+                        break;
+                    case NetworkMessages.SwitchButtonClick:
+                        button.Terminal.DoSwitchEvent();
+                        break;
+                }
+            }
+        }
+
+
+        private void RotateLeftButton_Click(Button sender)
+        {
+            button.Client_SendWindowData((UInt16)NetworkMessages.RotateLeftButtonClick);
+        }
+
+        private void RotateRightButton_Click(Button sender)
+        {
+            button.Client_SendWindowData((UInt16)NetworkMessages.RotateRightButtonClick);
+        }
+
+        private void SwitchButton_Click(Button sender)
+        {
+            button.Client_SendWindowData((UInt16)NetworkMessages.SwitchButtonClick);
+        }
+
+
+    }
+}
