75f20f420dcd0d426e4cf59ae7bf96e186b61a3a
j_wiel04@IVV5NET.WWU.DE <j_wiel04@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-07-07 15:07:50 +0000

    Schere Stein Papier Duell Rätsel.. noch nicht 100% fertig

---
+++ Projekt/Src/ProjectEntities/Client_SchereSteinPapierWindow.cs
@@ -0,0 +1,203 @@
+﻿using Engine.EntitySystem;
+using Engine.MathEx;
+using Engine.UISystem;
+using ProjectCommon;
+using System;
+using System.Collections.Generic;
+using System.Timers;
+namespace ProjectEntities
+{
+    public class Client_SchereSteinPapierWindow : TaskWindow
+    {
+        enum NetworkMessages
+        {
+            Client_SchereButtonClicked,
+            Client_PapierButtonClicked,
+            Client_SteinButtonClicked,
+            Client_PlayButtonClicked,   //create window for alien
+            Server_UpdateTimer,
+            Server_ClientLoses,
+            Server_ClientWins,
+            Server_Draw,
+            Server_EvaluatingSolutions  //disable Buttons
+        }
+
+        private EngineConsole console = EngineConsole.Instance;
+
+        private Control window;
+        private TextBox countdownBox, enemySelectedBox;
+        private Button schereButton, steinButton, papierButton, playButton;
+        private string lastSelected;
+
+        public Client_SchereSteinPapierWindow(Task task) : base(task)
+        {
+            window = ControlDeclarationManager.Instance.CreateControl("GUI\\Tasks\\SchereSteinPapier.gui");
+            Controls.Add(window);
+
+            schereButton = (Button)window.Controls["SchereButton"];
+            steinButton = (Button)window.Controls["SteinButton"];
+            papierButton = (Button)window.Controls["PapierButton"];
+            playButton = (Button)window.Controls["PlayButton"];
+            enemySelectedBox = (TextBox)window.Controls["GegnerWahl"];
+            countdownBox = (TextBox)window.Controls["Countdown"];
+
+            schereButton.Click += Schere_clicked;
+            papierButton.Click += Papier_clicked;
+            steinButton.Click += Stein_clicked;
+            playButton.Click += Play_clicked;
+
+            enemySelectedBox.Visible = false;
+
+            if (!task.IsServer)
+            {
+                task.Client_WindowStringReceived += Client_StringReceived;
+                task.Client_WindowDataReceived += Client_WindowDataReceived;
+            }
+                
+        }
+
+        
+
+        private void Play_clicked(Button sender)
+        {
+            task.Client_SendWindowData((UInt16)NetworkMessages.Client_PlayButtonClicked);
+            playButton.Visible = false;
+        }
+
+        
+
+        private void Stein_clicked(Button sender)
+        {
+            lastSelected = steinButton.Text;
+
+            task.Client_SendWindowData((UInt16)NetworkMessages.Client_SteinButtonClicked);
+        }
+
+        private void Papier_clicked(Button sender)
+        {
+            lastSelected = steinButton.Text;
+
+            task.Client_SendWindowData((UInt16)NetworkMessages.Client_PapierButtonClicked);
+        }
+
+        private void Schere_clicked(Button sender)
+        {
+            lastSelected = steinButton.Text;
+
+            task.Client_SendWindowData((UInt16)NetworkMessages.Client_SchereButtonClicked);
+        }
+
+        private void victoryStuff()
+        {
+            switch(lastSelected)
+            {
+                case "Schere":
+                    enemySelectedBox.Text = papierButton.Text;
+                    break;
+                case "Papier":
+                    enemySelectedBox.Text = steinButton.Text;
+                    break;
+                case "Stein":
+                    enemySelectedBox.Text = schereButton.Text;
+                    break;
+            }
+
+            task.Success = true;
+        }
+
+        private void drawStuff()
+        {
+            switch (lastSelected)
+            {
+                case "Schere":
+                    enemySelectedBox.Text = schereButton.Text;
+                    break;
+                case "Papier":
+                    enemySelectedBox.Text = papierButton.Text;
+                    break;
+                case "Stein":
+                    enemySelectedBox.Text = steinButton.Text;
+                    break;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private void defeatStuff()
+        {
+            switch (lastSelected)
+            {
+                case "Schere":
+                    enemySelectedBox.Text = steinButton.Text;
+                    break;
+                case "Papier":
+                    enemySelectedBox.Text = schereButton.Text;
+                    break;
+                case "Stein":
+                    enemySelectedBox.Text = papierButton.Text;
+                    break;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private void Client_StringReceived(string message, UInt16 netMessage)
+        {
+            if (!task.IsServer)
+            {
+                NetworkMessages msg = (NetworkMessages)netMessage;
+                switch (msg)
+                {
+                    case NetworkMessages.Server_UpdateTimer:
+                        countdownBox.Text = message;
+                        break;
+
+                }
+            }
+        }
+
+        private void Client_WindowDataReceived(ushort message)
+        {
+            if(!task.IsServer)
+            {
+                NetworkMessages msg = (NetworkMessages)message;
+                switch(msg)
+                {
+                    case NetworkMessages.Server_Draw:
+                        drawStuff();
+                        break;
+                    case NetworkMessages.Server_ClientWins:
+                        victoryStuff();
+                        break;
+                    case NetworkMessages.Server_ClientLoses:
+                        defeatStuff();
+                        break;
+                    case NetworkMessages.Server_EvaluatingSolutions:
+                        schereButton.Enable = false;
+                        steinButton.Enable = false;
+                        papierButton.Enable = false;
+                        break;
+                }
+            }
+        }
+
+        private void Server_WindowDataReceived(ushort message)
+        {
+            if(task.IsServer)
+            {
+                NetworkMessages msg = (NetworkMessages)message;
+                switch(msg)
+                {
+                    case NetworkMessages.Client_PlayButtonClicked:
+                        Server_createWindowForAlien();
+                        break;
+                }
+            }
+        }
+
+        private void Server_createWindowForAlien()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Projekt/Src/ProjectEntities/Server_SchereSteinPapierWindow.cs
@@ -0,0 +1,162 @@
+﻿using Engine.EntitySystem;
+using Engine.MathEx;
+using Engine.UISystem;
+using ProjectCommon;
+using System;
+using System.Collections.Generic;
+using System.Timers;
+namespace ProjectEntities
+{
+    public class Server_SchereSteinPapierWindow : TaskWindow
+    {
+        enum NetworkMessages
+        {
+            Client_SchereButtonClicked,
+            Client_PapierButtonClicked,
+            Client_SteinButtonClicked,
+            Client_PlayButtonClicked,   //not for this class
+            Server_UpdateTimer,
+            Server_ClientLoses,
+            Server_ClientWins,
+            Server_Draw,
+            Server_EvaluatingSolutions
+        }
+
+        private EngineConsole console = EngineConsole.Instance;
+
+        private Control window;
+        private TextBox countdownBox, enemySelectedBox;
+        private Button schereButton, steinButton, papierButton;
+        private string lastSelected, enemyLastSelected;
+        private Timer aTimer;
+
+        public Server_SchereSteinPapierWindow(Task task) : base(task)
+        {
+            window = ControlDeclarationManager.Instance.CreateControl("GUI\\Tasks\\SchereSteinPapier.gui");
+            Controls.Add(window);
+
+            schereButton = (Button)window.Controls["SchereButton"];
+            steinButton = (Button)window.Controls["SteinButton"];
+            papierButton = (Button)window.Controls["PapierButton"];
+            enemySelectedBox = (TextBox)window.Controls["GegnerWahl"];
+            countdownBox = (TextBox)window.Controls["Countdown"];
+
+            schereButton.Click += Schere_clicked;
+            papierButton.Click += Papier_clicked;
+            steinButton.Click += Stein_clicked;
+
+            schereButton.Enable = true;
+            steinButton.Enable = true;
+            papierButton.Enable = true;
+            enemySelectedBox.Visible = false;
+
+            if (task.IsServer)
+            {
+                task.Server_WindowDataReceived += Server_WindowDataReceived;
+                aTimer = new System.Timers.Timer(1000); //jede Sekunde
+                aTimer.Elapsed += countdown;
+            }
+                
+        }
+
+        private void countdown(object sender, ElapsedEventArgs e)
+        {
+            countdownBox.Text = ""+(int.Parse(countdownBox.Text) -1);
+            task.Server_SendWindowString(countdownBox.Text, (UInt16)NetworkMessages.Server_UpdateTimer);
+            if (countdownBox.Text.Equals("0"))
+                compareSolutions();
+        }   
+        
+
+        private void Stein_clicked(Button sender)
+        {
+            if(!(countdownBox.Text.Equals("0")))
+                lastSelected = steinButton.Text;
+        }
+
+        private void Papier_clicked(Button sender)
+        {
+            if (!(countdownBox.Text.Equals("0")))
+                lastSelected = steinButton.Text;
+        }
+
+        private void Schere_clicked(Button sender)
+        {
+            if (!(countdownBox.Text.Equals("0")))
+                lastSelected = steinButton.Text;
+        }
+
+        private void compareSolutions()
+        {
+            task.Server_SendWindowData((UInt16)NetworkMessages.Server_EvaluatingSolutions);
+
+            schereButton.Enable = false;
+            papierButton.Enable = false;
+            steinButton.Enable = false;
+
+            switch(lastSelected)
+            {
+                case "Schere":
+                    if (enemyLastSelected.Equals("Stein"))
+                        defeatStuff();
+                    if (enemyLastSelected.Equals("Schere"))
+                        drawStuff();
+                    if (enemyLastSelected.Equals("Papier"))
+                        victoryStuff();
+                    break;
+                case "Papier":
+                    if (enemyLastSelected.Equals("Schere"))
+                        defeatStuff();
+                    if (enemyLastSelected.Equals("Papier"))
+                        drawStuff();
+                    if (enemyLastSelected.Equals("Stein"))
+                        victoryStuff();
+                    break;
+                case "Stein":
+                    if (enemyLastSelected.Equals("Papier"))
+                        defeatStuff();
+                    if (enemyLastSelected.Equals("Stein"))
+                        drawStuff();
+                    if (enemyLastSelected.Equals("Schere"))
+                        victoryStuff();
+                    break;
+            }
+                
+        }
+
+        private void victoryStuff()
+        {
+            task.Server_SendWindowData((UInt16)NetworkMessages.Server_ClientLoses);
+        }
+
+        private void drawStuff()
+        {
+            task.Server_SendWindowData((UInt16)NetworkMessages.Server_Draw);
+        }
+
+        private void defeatStuff()
+        {
+            task.Server_SendWindowData((UInt16)NetworkMessages.Server_ClientWins);
+        }
+
+        private void Server_WindowDataReceived(ushort message)
+        {
+            if(!task.IsServer)
+            {
+                NetworkMessages msg = (NetworkMessages)message;
+                switch(msg)
+                {
+                    case NetworkMessages.Client_PapierButtonClicked:
+                        enemyLastSelected = papierButton.Text;
+                        break;
+                    case NetworkMessages.Client_SchereButtonClicked:
+                        enemyLastSelected = schereButton.Text;
+                        break;
+                    case NetworkMessages.Client_SteinButtonClicked:
+                        enemyLastSelected = steinButton.Text;
+                        break;
+                }
+            }
+        }
+    }
+}
