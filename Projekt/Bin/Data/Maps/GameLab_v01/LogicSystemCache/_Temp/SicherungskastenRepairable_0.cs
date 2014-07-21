c0b5e635e2ae3ab554c2702f57e9a74471d2abf6
m_scho52@IVV5NET.WWU.DE <m_scho52@IVV5NET.WWU.DE@d33c1221-daf4-4eae-9122-ee141962ec48>
2014-07-21 11:35:49 +0000

    Closed issue 79

---
+++ Projekt/Bin/Data/Maps/GameLab_v01/LogicSystemCache/_Temp/SicherungskastenRepairable_0.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine;
+using Engine.FileSystem;
+using Engine.MathEx;
+using Engine.Utils;
+using Engine.Renderer;
+using Engine.PhysicsSystem;
+using Engine.SoundSystem;
+using Engine.UISystem;
+using Engine.EntitySystem;
+using Engine.MapSystem;
+using ProjectCommon;
+using ProjectEntities;
+
+namespace Maps_GameLab_v___LogicSystem_LogicSystemScripts
+{
+	public class SicherungskastenRepairable_0 : Engine.EntitySystem.LogicSystem.LogicEntityObject
+	{
+		ProjectEntities.Repairable __ownerEntity;
+		
+		public SicherungskastenRepairable_0( ProjectEntities.Repairable ownerEntity )
+			: base( ownerEntity )
+		{
+			this.__ownerEntity = ownerEntity;
+		}
+		
+		public ProjectEntities.Repairable Owner
+		{
+			get { return __ownerEntity; }
+		}
+		
+		
+	}
+}
---
+++ Projekt/Bin/Data/Maps/GameLab_v01/LogicSystemCache/_Temp/Switch_Ventilator.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine;
+using Engine.FileSystem;
+using Engine.MathEx;
+using Engine.Utils;
+using Engine.Renderer;
+using Engine.PhysicsSystem;
+using Engine.SoundSystem;
+using Engine.UISystem;
+using Engine.EntitySystem;
+using Engine.MapSystem;
+using ProjectCommon;
+using ProjectEntities;
+
+namespace Maps_GameLab_v___LogicSystem_LogicSystemScripts
+{
+	public class Switch_Ventilator : Engine.EntitySystem.LogicSystem.LogicEntityObject
+	{
+		ProjectEntities.BooleanSwitch __ownerEntity;
+		
+		public Switch_Ventilator( ProjectEntities.BooleanSwitch ownerEntity )
+			: base( ownerEntity )
+		{
+			this.__ownerEntity = ownerEntity;
+			ownerEntity.ValueChange += delegate( ProjectEntities.Switch __entity ) { if( Engine.EntitySystem.LogicSystemManager.Instance != null )ValueChange(  ); };
+		}
+		
+		public ProjectEntities.BooleanSwitch Owner
+		{
+			get { return __ownerEntity; }
+		}
+		
+		
+		public void fanControll( ProjectEntities.BooleanSwitch param0 )
+		{
+			//Fan fan = Fan_0 = Fan;
+			//if (param0 == True) {
+			//	fan.Throttle = 0.5;
+			//}
+			//Throttle [-1,1]
+		}
+
+		public void ValueChange()
+		{
+			if(Owner.Value)
+				((Fan)Entities.Instance.GetByName("Fan_0")).Throttle = 1;
+			else
+				((Fan)Entities.Instance.GetByName("Fan_0")).Throttle = 0;
+		}
+
+	}
+}
